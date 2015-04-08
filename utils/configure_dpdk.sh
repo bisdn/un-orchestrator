ff41428b34e5c3c8cb1615d97e60eb955c31f016
ivanocerrato <e@example.com>
2015-04-08 16:50:40 +0200

    Updated the script to configure the DPDK on the UN.

--- utils/configure_dpdk.sh
+++ utils/configure_dpdk.sh
@@ -1,36 +1,38 @@
 #! /bin/bash
 
 # Author: Ivano Cerrato
-# Date: 17/04/2014
+# Date: 08/05/2015
 #
 # Brief: this script configures the DPDK on the machine. In other words, it loads
 #    the proper kernel modules, associates some interfaces to the DPDK, and finally
-#    creates a number of huge pages. The inetrafeces to be assocaited with the DPDP,
+#    creates a number of huge pages. The interfaces to be assocaited with the DPDK,
 #    as well as the number of huge pages to be created can be easily changed by
 #    modifying this script.
-#    Note that the script requires that DPDK have been properly compiled.
-#    Instead of this script, you can use the interactive one provided with the DPDK.
+#    Note that the script requires that DPDK have been properly compiled, and 
+#    refers to the DPDK version available at https://github.com/bisdn/dpdk-dev .
+#    Instead of this script, you can use the interactive tool provided with the 
+#    DPDK repository.
 
 #Set properly the next row, to point to the DPDK folder
-cd ~/Scrivania/dpdk-1.6.0r2
+cd ~/Desktop/dpdk-dev
 
 sudo modprobe uio
 sudo insmod build/kmod/igb_uio.ko
 sudo insmod build/kmod/rte_kni.ko
 
-sudo ifconfig eth0 down
-sudo tools/igb_uio_bind.py --bind=igb_uio eth0
+sudo ifconfig em2 down
+sudo tools/dpdk_nic_bind.py --bind=igb_uio em2
 
-sudo ifconfig eth1 down
-sudo tools/igb_uio_bind.py --bind=igb_uio eth1
+sudo ifconfig em3 down
+sudo tools/dpdk_nic_bind.py --bind=igb_uio em3
 
 #Add here other interfaces to be managed with DPDK
 
 #Set properly the next row, to point to the DPDK folder
-export RTE_SDK=~/Scrivania/dpdk-1.6.0r2/build
+export RTE_SDK=~/Desktop/UNutils/dpdk-dev/x86_64-native-linuxapp-gcc/
 
 sudo -u root  mkdir -p /mnt/huge
 sudo -u root  mount -t hugetlbfs nodev /mnt/huge
 #Change the next row to allocate a different number of huge pages
-sudo -u root  echo 500 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+sudo -u root  echo 1000 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 
