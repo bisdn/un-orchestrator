a58d65351f8ebccaf7a9620250a3b0ace8053737
David Verbeiren <david.verbeiren@intel.com>
2015-07-06 18:49:17 +0200

    Added OVS DPDK switch manager plugin (not complete)

---
+++ orchestrator/network_controller/switch_manager/plugins/ovs-dpdk/ovsdpdk_manager.cc
@@ -0,0 +1,156 @@
+#include "ovsdpdk_manager.h"
+
+OVSDPDKManager::OVSDPDKManager() : m_NextLsiId(0), m_NextPortId(1) /* 0 is not valid for OVS */
+{
+}
+
+OVSDPDKManager::~OVSDPDKManager()
+{
+}
+
+void OVSDPDKManager::checkPhysicalInterfaces(set<CheckPhysicalPortsIn> cppi)
+{ // SwitchManager implementation
+	//logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "checkPhysicalInterfaces(dpid: %" PRIu64 " NF:%s NFType:%d)\n", anpi.getDpid(), anpi.getNFname().c_str(), anpi.getNFtype());
+}
+
+CreateLsiOut *OVSDPDKManager::createLsi(CreateLsiIn cli)
+{  // SwitchManager implementation
+
+	unsigned int dpid = m_NextLsiId++;
+
+	logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "createLsi() creating LSI %d", dpid);
+
+	stringstream cmd;
+	cmd << CMD_CREATE_LSI << " " << dpid << " " << cli.getControllerAddress() << " " << cli.getControllerPort() << " ";
+	// Set the OpenFlow version
+	switch(OFP_VERSION) {
+		case OFP_10:
+			cmd << "OpenFlow10";
+			break;
+		case OFP_12:
+			cmd << "OpenFlow12";
+			break;
+		case OFP_13:
+			cmd << "OpenFlow13";
+			break;
+	}
+	logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "Executing command \"%s\"", cmd.str().c_str());
+	int retVal = system(cmd.str().c_str());
+	retVal = retVal >> 8;
+	if(retVal == 0) {
+		logger(ORCH_WARNING, MODULE_NAME, __FILE__, __LINE__, "Failed to create LSI");
+		throw OVSDPDKManagerException();
+	}
+
+	// Add ports
+	list<string> ports = cli.getPhysicalPortsName();
+	typedef map<string,unsigned int> PortsNameIdMap;
+	PortsNameIdMap out_physical_ports;
+
+	list<string>::iterator pit = ports.begin();
+	for(; pit != ports.end(); pit++)
+	{
+		// Go create it!
+		unsigned int port_id = m_NextPortId++;
+		logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, " phy port \"%s\" = %d", pit->c_str(), port_id);
+		stringstream cmd_add;
+		cmd_add << CMD_ADD_PORT << " " << dpid << " " << *pit << " " << "dpdk" << " " << port_id;
+		logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "Executing command \"%s\"", cmd_add.str().c_str());
+		int retVal = system(cmd_add.str().c_str());
+		retVal = retVal >> 8;
+		if(retVal == 0) {
+			logger(ORCH_WARNING, MODULE_NAME, __FILE__, __LINE__, "Failed to add port");
+			throw OVSDPDKManagerException();
+		}
+		// TODO - Really check result!
+		out_physical_ports.insert(PortsNameIdMap::value_type(*pit, port_id));
+	}
+
+	// Add NF ports
+	typedef map<string,PortsNameIdMap > NfPortsMapMap;
+	map<string,nf_t> nf_types = cli.getNetworkFunctionsType();
+	NfPortsMapMap out_nf_ports;
+	list<pair<unsigned int, unsigned int> > out_virtual_links;
+	set<string> nfs = cli.getNetworkFunctionsName();
+	for(set<string>::iterator nf = nfs.begin(); nf != nfs.end(); nf++) {
+		nf_t nf_type = nf_types[*nf];
+		list<string> nf_ports = cli.getNetworkFunctionsPortNames(*nf);
+		PortsNameIdMap nf_ports_ids;
+		for(list<string>::iterator nfp = nf_ports.begin(); nfp != nf_ports.end(); nfp++) {
+			unsigned int port_id = m_NextPortId++;
+			const char* port_type = "dpdkvhostuser";  // TODO - dpdkr, dpdkvhostuser, tap, virtio ...
+			logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, " NF port \"%s.%s\" = %d (type=%d)", nf->c_str(), nfp->c_str(), port_id, nf_type);
+			stringstream cmd_add;
+			cmd_add << CMD_ADD_PORT << " " << dpid << " " << *nfp << " " << port_type << " " << port_id;
+			logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "Executing command \"%s\"", cmd_add.str().c_str());
+			int retVal = system(cmd_add.str().c_str());
+			retVal = retVal >> 8;
+			if(retVal == 0) {
+				logger(ORCH_WARNING, MODULE_NAME, __FILE__, __LINE__, "Failed to add port");
+				throw OVSDPDKManagerException();
+			}
+			// TODO - Really check result!
+			nf_ports_ids.insert(PortsNameIdMap::value_type(*nfp, port_id));
+		}
+		out_nf_ports.insert(NfPortsMapMap::value_type(*nf, nf_ports_ids));
+	}
+
+	// Add Ports for Virtual Links (patch ports)
+	list<uint64_t> vlinks = cli.getVirtualLinksRemoteLSI();
+	for(list<uint64_t>::iterator vl = vlinks.begin(); vl != vlinks.end(); vl++) {
+
+		unsigned int s_port_id = m_NextPortId++;
+		unsigned int d_port_id = m_NextPortId++;
+
+		logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, " Virtual link to LSI %u: %u:%u <-> %u:%u", *vl, dpid, s_port_id, *vl, d_port_id);
+		stringstream cmd_add;
+		cmd_add << CMD_VIRTUAL_LINK << " " << dpid << " " << *vl << " " << s_port_id << " " << d_port_id;
+		logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "Executing command \"%s\"", cmd_add.str().c_str());
+		int retVal = system(cmd_add.str().c_str());
+		retVal = retVal >> 8;
+		if(retVal == 0) {
+			logger(ORCH_WARNING, MODULE_NAME, __FILE__, __LINE__, "Failed to create virtual link");
+			throw OVSDPDKManagerException();
+		}
+
+		out_virtual_links.push_back(make_pair(s_port_id, d_port_id));
+	}
+
+	CreateLsiOut *clo = new CreateLsiOut(dpid, out_physical_ports, out_nf_ports, out_virtual_links);
+	return clo;
+}
+
+AddNFportsOut *OVSDPDKManager::addNFPorts(AddNFportsIn anpi)
+{ // SwitchManager implementation
+  
+	AddNFportsOut *anpo = NULL;
+	logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "addNFPorts(dpid: %" PRIu64 " NF:%s NFType:%d)\n", anpi.getDpid(), anpi.getNFname().c_str(), anpi.getNFtype());
+	list<string> nfs_ports = anpi.getNetworkFunctionsPorts();
+	for(list<string>::iterator nfp = nfs_ports.begin(); nfp != nfs_ports.end(); nfp++) {
+		logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "\tport: %s\n", (*nfp).c_str());
+	}
+	return anpo;
+}
+
+AddVirtualLinkOut *OVSDPDKManager::addVirtualLink(AddVirtualLinkIn avli)
+{ // SwitchManager implementation
+	AddVirtualLinkOut *avlo = NULL;
+	logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "addVirtualLink(dpid: %" PRIu64 " -> %" PRIu64 ")\n", avli.getDpidA(), avli.getDpidB());
+	return avlo;
+}
+
+void OVSDPDKManager::destroyLsi(uint64_t dpid)
+{ // SwitchManager implementation
+	logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "destroyLsi(dpid: %" PRIu64 " -> %" PRIu64 ")\n", dpid);
+}
+
+void OVSDPDKManager::destroyVirtualLink(DestroyVirtualLinkIn dvli)
+{ // SwitchManager implementation
+	logger(ORCH_DEBUG_INFO, MODULE_NAME, __FILE__, __LINE__, "destroyVirtualLink(%" PRIu64 ".%" PRIu64 " -> %" PRIu64 ".%" PRIu64 ")\n",
+			dvli.getDpidA(), dvli.getIdA(), dvli.getDpidB(), dvli.getIdB());
+
+}
+
+void OVSDPDKManager::destroyNFPorts(DestroyNFportsIn dnpi)
+{ // SwitchManager implementation
+}
---
+++ orchestrator/network_controller/switch_manager/plugins/ovs-dpdk/scripts/VirtualLink.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Author: UNIFY Consortium
+#Date: 2015-07-05 
+#Brief: Add a Virtual Link between two bridges (using patch ports)
+
+s_br=`echo br_$1`
+d_br=`echo br_$2`
+s_port_name=`echo VPort$1_$2`
+d_port_name=`echo VPort$2_$1`
+s_port_id=$3
+d_port_id=$4
+
+. ./network_controller/switch_manager/plugins/ovs-dpdk/scripts/ovs.conf
+
+if (( $EUID != 0 ))
+then
+    echo "[$0] This script must be executed with ROOT privileges"
+    exit 0
+fi
+
+VSCTL="$OVS_DIR/utilities/ovs-vsctl"
+
+$VSCTL add-port $s_br $s_port_name -- set Interface $s_port_name type=patch ofport_request=$s_port_id options:peer=$d_port_name
+$VSCTL add-port $d_br $d_port_name -- set Interface $d_port_name type=patch ofport_request=$d_port_id options:peer=$s_port_name
+
+exit 1
